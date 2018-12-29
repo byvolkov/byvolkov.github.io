4e055b83a2567ff1c4421c2a99f215cbc189ffd0
Евгений <31438864+YoungGolden@users.noreply.github.com>
2018-12-29 22:34:32 +0300

    d
    d

--- form/rest.php
+++ form/rest.php
@@ -5,7 +5,7 @@ define('CRM_PORT', '443'); // CRM server port
 define('CRM_PATH', '/crm/configs/import/lead.php'); // CRM server REST service path
 
 // CRM server authorization data
-define('CRM_LOGIN', 'evgeniivolkoff@gmail.com'); // login of a CRM user able to manage leads
+define('CRM_LOGIN', 'airdropworks@gmail.com'); // login of a CRM user able to manage leads
 define('CRM_PASSWORD', '842684265RTTS'); // password of a CRM user
 // OR you can send special authorization hash which is sent by server after first successful connection with login and password
 //define('CRM_AUTH', 'e54ec19f0c5f092ea11145b80f465e1a'); // authorization hash
