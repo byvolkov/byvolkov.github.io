06955cc914b6f30be04b46aaf7d00518508e783f
Евгений <31438864+YoungGolden@users.noreply.github.com>
2018-12-29 22:25:54 +0300

    d
    d

--- form/rest.php
+++ form/rest.php
@@ -1,12 +1,12 @@
 <?
 // CRM server conection data
-define('CRM_HOST', 'b24-k6ubec.bitrix24.ru'); // your CRM domain name
+define('CRM_HOST', 'b24-ob3999.bitrix24.ru'); // your CRM domain name
 define('CRM_PORT', '443'); // CRM server port
 define('CRM_PATH', '/crm/configs/import/lead.php'); // CRM server REST service path
 
 // CRM server authorization data
 define('CRM_LOGIN', 'evgeniivolkoff@gmail.com'); // login of a CRM user able to manage leads
-//define('CRM_PASSWORD', 'password'); // password of a CRM user
+define('CRM_PASSWORD', '842684265RTTS'); // password of a CRM user
 // OR you can send special authorization hash which is sent by server after first successful connection with login and password
 //define('CRM_AUTH', 'e54ec19f0c5f092ea11145b80f465e1a'); // authorization hash
 
@@ -21,7 +21,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
     $postData = array(
         'TITLE' => $leadData['TITLE'],// инфо, откуда приходят заявки клиентов
         'NAME' => $leadData['NAME'], // имя клиента
-        'EMAIL_WORK' =>$leadData['COMMENTS'], // эмейл клиента
+        'EMAIL_WORK' =>$leadData['EMAIL'], // эмейл клиента
         'PHONE_WORK' =>$_POST['PHONE'], // сохраняем телефон
         'COMMENTS' => $leadData['COMMENTS'],// комментарий клиента
     );
