6f880d38434816550fedc33067f87c399626b07e
Евгений <31438864+YoungGolden@users.noreply.github.com>
2018-12-30 19:41:36 +0300

    +
    +

--- rest_b24/rest.php
+++ rest_b24/rest.php
@@ -1,78 +1,98 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Страница благодарности</title>
-<link href="css/style.css" type="text/css" rel="stylesheet"/>
-</head>
-<body>
+<?
+// CRM server conection data
+define('CRM_HOST', 'your_domain.bitrix24.com'); // your CRM domain name
+define('CRM_PORT', '443'); // CRM server port
+define('CRM_PATH', '/crm/configs/import/lead.php'); // CRM server REST service path
 
-<?php
-define('CRM_HOST', 'b24-k6ubec.bitrix24.ru'); // Домен срм системы
-define('CRM_PORT', '443'); 
-define('CRM_PATH', '/crm/configs/import/lead.php'); 
-define('CRM_LOGIN', 'evgeniivolkoff@gmail.com');  // логин
-//define('CRM_PASSWORD', 'zyeZcJqbFp'); // пароль
+// CRM server authorization data
+define('CRM_LOGIN', 'login'); // login of a CRM user able to manage leads
+define('CRM_PASSWORD', 'password'); // password of a CRM user
+// OR you can send special authorization hash which is sent by server after first successful connection with login and password
+//define('CRM_AUTH', 'e54ec19f0c5f092ea11145b80f465e1a'); // authorization hash
 
 /********************************************************************************************/
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+// POST processing
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+	$leadData = $_POST['DATA'];
 
-    // получаем данные из полей и задаем название лида
-    
-    $postData = array(
-        'TITLE' => $_POST['metka'], // сохраняем нашу метку и формируем заголовок лида
-        'NAME' => $_POST['name'],   // сохраняем имя
-        'PHONE_WORK' =>$_POST['phone'], // сохраняем телефон
-    );
+	// get lead data from the form
+	$postData = array(
+		'TITLE' => $leadData['TITLE'],
+		'COMPANY_TITLE' => $leadData['COMPANY_TITLE'],
+		'NAME' => $leadData['NAME'],
+		'LAST_NAME' => $leadData['LAST_NAME'],
+		'COMMENTS' => $leadData['COMMENTS'],
+	);
 
-    // авторизация, проверка логина и пароля
-    if (defined('CRM_AUTH'))
-    {
-        $postData['AUTH'] = CRM_AUTH;
-    }
-    else
-    {
-        $postData['LOGIN'] = CRM_LOGIN;
-        $postData['PASSWORD'] = CRM_PASSWORD;
-    }
+	// append authorization data
+	if (defined('CRM_AUTH'))
+	{
+		$postData['AUTH'] = CRM_AUTH;
+	}
+	else
+	{
+		$postData['LOGIN'] = CRM_LOGIN;
+		$postData['PASSWORD'] = CRM_PASSWORD;
+	}
 
-    $fp = fsockopen("ssl://".CRM_HOST, CRM_PORT, $errno, $errstr, 30);
-    if ($fp)
-    {
-        // формируем и шифруем строку с данными из формы
-        $strPostData = '';
-        foreach ($postData as $key => $value)
-            $strPostData .= ($strPostData == '' ? '' : '&').$key.'='.urlencode($value);
-            $str = "POST ".CRM_PATH." HTTP/1.0\r\n";
-            $str .= "Host: ".CRM_HOST."\r\n";
-            $str .= "Content-Type: application/x-www-form-urlencoded\r\n";
-            $str .= "Content-Length: ".strlen($strPostData)."\r\n";
-            $str .= "Connection: close\r\n\r\n";
+	// open socket to CRM
+	$fp = fsockopen("ssl://".CRM_HOST, CRM_PORT, $errno, $errstr, 30);
+	if ($fp)
+	{
+		// prepare POST data
+		$strPostData = '';
+		foreach ($postData as $key => $value)
+			$strPostData .= ($strPostData == '' ? '' : '&').$key.'='.urlencode($value);
 
-        $str .= $strPostData;
+		// prepare POST headers
+		$str = "POST ".CRM_PATH." HTTP/1.0\r\n";
+		$str .= "Host: ".CRM_HOST."\r\n";
+		$str .= "Content-Type: application/x-www-form-urlencoded\r\n";
+		$str .= "Content-Length: ".strlen($strPostData)."\r\n";
+		$str .= "Connection: close\r\n\r\n";
 
-        // отправляем запрос в срм систему
-        fwrite($fp, $str );
-        $result = '';
-        while (!feof($fp))
-        {
-            $result .= fgets($fp, 128);
-        }
-        fclose($fp);
-        $response = explode("\r\n\r\n", $result);
-        $output = '<pre>'.print_r($response[1], 1).'</pre>';
-    }
-    else
-    {
-        echo 'Connection Failed! '.$errstr.' ('.$errno.')';
-    }
+		$str .= $strPostData;
+
+		// send POST to CRM
+		fwrite($fp, $str);
+
+		// get CRM headers
+		$result = '';
+		while (!feof($fp))
+		{
+			$result .= fgets($fp, 128);
+		}
+		fclose($fp);
+
+		// cut response headers
+		$response = explode("\r\n\r\n", $result);
+
+		$output = '<pre>'.print_r($response[1], 1).'</pre>';
+	}
+	else
+	{
+		echo 'Connection Failed! '.$errstr.' ('.$errno.')';
+	}
 }
-//else
-// {
-//     $output = 'Ошибка';
-// }
-?>
+else
+{
+	$output = '';
+}
 
-</body>
-</html>
+// HTML form
+?>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<?=$output;?>
+<form action="rest.php" method="post">
+	Title*: <input type="text" name="DATA[TITLE]" value="" /><br />
+	Company Name: <input type="text" name="DATA[COMPANY_TITLE]" value="" /><br />
+	First Name: <input type="text" name="DATA[NAME]" value="" /><br />
+	Last Name: <input type="text" name="DATA[LAST_NAME]" value="" /><br />
+	Comments: <textarea name="DATA[COMMENTS]"></textarea><br />
+	<input type="submit" value="Send" />
+</form>
